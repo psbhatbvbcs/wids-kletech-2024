147ccd0bf0429757075b3448afdce34191875cc1
Pranav Bhat <96832303+psbhatbvbcs@users.noreply.github.com>
2024-04-07 11:46:24 +0530

    [done] - version 1.0

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,6 +6,12 @@ const Footer = () => {
       <div className="w-full bg-[#4AB969] text-xl p-4 flex justify-center font-normal md:text-xl">
         KLE Technological University, Huballi
       </div>
+      <p className="w-full bg-[#4AB969] flex justify-center font-medium pb-2">
+        <a href="https://wids.kletech.ac.in/" target="_blank">
+          <span className="font-medium">Archive - </span>WiDS Datathon Workshop
+          2023
+        </a>
+      </p>
     </div>
   );
 };
